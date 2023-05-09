7c80f8fcc7240a9ec60c14332584d330a16321ff
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-09 11:44:31 +0530

    Initial Version of domain

---
+++ TicketsManagementSystem/src/Core/TicketsManagement.Domain/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using TicketsManagement.Domain.Common;
+
+namespace TicketsManagement.Domain.Entities;
+
+public class Category : AuditableEntity
+{
+    public Guid CategoryId { get; set; }
+
+    public string? Name { get; set; }
+
+    public ICollection<Event>? Events { get; set; }
+}
