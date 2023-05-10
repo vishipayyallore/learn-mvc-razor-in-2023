0aa3119b3041723a4edd7d23eada0ad7438e79d5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-05-10 13:26:47 +0530

    few Changes

--- TicketsManagementSystem/src/Core/TicketsManagement.Application/Profiles/MappingProfile.cs
+++ TicketsManagementSystem/src/Core/TicketsManagement.Application/Profiles/MappingProfile.cs
@@ -5,7 +5,6 @@ using TicketsManagement.Application.Features.Categories.Queries.GetCategoriesLis
 using TicketsManagement.Application.Features.Events.Commands.CreateEvent;
 using TicketsManagement.Application.Features.Events.Commands.UpdateEvent;
 using TicketsManagement.Application.Features.Events.Queries.GetEventDetail;
-using TicketsManagement.Application.Features.Events.Queries.GetEventsExport;
 using TicketsManagement.Application.Features.Events.Queries.GetEventsList;
 using TicketsManagement.Application.Features.Orders.GetOrdersForMonth;
 using TicketsManagement.Domain.Entities;
@@ -21,7 +20,6 @@ namespace TicketsManagement.Application.Profiles
             CreateMap<Event, UpdateEventCommand>().ReverseMap();
             CreateMap<Event, EventDetailVm>().ReverseMap();
             CreateMap<Event, CategoryEventDto>().ReverseMap();
-            CreateMap<Event, EventExportDto>().ReverseMap();
 
             CreateMap<Category, CategoryDto>();
             CreateMap<Category, CategoryListVm>();
