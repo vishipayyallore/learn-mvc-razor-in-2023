2a9e7ef55ced5fb4ba47290f0581d230ad064b59
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-14 22:03:22 +0530

    Create ProductsController.cs

---
+++ Coffee.eShop/Coffee.eShop/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Coffee.eShop.ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee.eShop.Controllers;
+
+public class ProductsController : Controller
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductsController(IProductRepository productRepository)
+    {
+        _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+    }
+
+    public IActionResult Shop()
+    {
+        return View(_productRepository.GetAllProducts());
+    }
+
+    public IActionResult Detail(int id)
+    {
+        var product = _productRepository.GetProductDetail(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+        return View(product);
+    }
+}
