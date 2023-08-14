aad92fd4b974f418770f3a36425cbe606e1282eb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-14 21:56:59 +0530

    Update ProductRepository.cs

--- Coffee.eShop/Coffee.eShop/Repositories/ProductRepository.cs
+++ Coffee.eShop/Coffee.eShop/Repositories/ProductRepository.cs
@@ -5,6 +5,13 @@ namespace Coffee.eShop.Repositories;
 
 public class ProductRepository : IProductRepository
 {
+    private readonly List<Product> _products = new()
+    {
+        new Product { Id = 1, Name = "Americano", Price = 10.01M, Detail = "Coffee Americano", IsTrendingProduct = false, ImageUrl = "https://unsplash.com/photos/nzyzAUsbV0M" },
+        new Product { Id = 2, Name = "Cortado", Price = 10.01M, Detail = "Coffee Cortado", IsTrendingProduct = true, ImageUrl = "https://unsplash.com/photos/6VhPY27jdps" },
+        new Product { Id = 3, Name = "Mocha", Price = 10.01M, Detail = "Coffee Mocha", IsTrendingProduct = true, ImageUrl = "https://unsplash.com/photos/IhqDpFz7I8Q" }
+    };
+
     //private CoffeeShopDbContext dbContext;
     //public ProductRepository(CoffeeShopDbContext dbContext)
     //{
@@ -13,16 +20,16 @@ public class ProductRepository : IProductRepository
 
     public IEnumerable<Product> GetAllProducts()
     {
-        return dbContext.Products;
+        return _products;
     }
 
     public Product? GetProductDetail(int id)
     {
-        return dbContext.Products.FirstOrDefault(p => p.Id == id);
+        return _products.FirstOrDefault(p => p.Id == id);
     }
 
     public IEnumerable<Product> GetTrendingProducts()
     {
-        return dbContext.Products.Where(p => p.IsTrendingProduct);
+        return _products.Where(p => p.IsTrendingProduct);
     }
 }
