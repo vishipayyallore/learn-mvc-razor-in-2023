0bb5a54024fbcbf77f03e5d75cd5238a8abc6b12
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-14 21:47:40 +0530

    Create ProductRepository.cs

---
+++ Coffee.eShop/Coffee.eShop/Repositories/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using Coffee.eShop.ApplicationCore.Interfaces;
+using Coffee.eShop.Models;
+
+namespace Coffee.eShop.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    //private CoffeeShopDbContext dbContext;
+    //public ProductRepository(CoffeeShopDbContext dbContext)
+    //{
+    //    this.dbContext = dbContext;
+    //}
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        return dbContext.Products;
+    }
+
+    public Product? GetProductDetail(int id)
+    {
+        return dbContext.Products.FirstOrDefault(p => p.Id == id);
+    }
+
+    public IEnumerable<Product> GetTrendingProducts()
+    {
+        return dbContext.Products.Where(p => p.IsTrendingProduct);
+    }
+}
