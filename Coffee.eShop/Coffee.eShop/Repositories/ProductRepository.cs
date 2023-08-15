cca60a8a9bb038d65b294166c9a03f3c305a11cd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-15 22:22:14 +0530

    Update ProductRepository.cs

--- Coffee.eShop/Coffee.eShop/Repositories/ProductRepository.cs
+++ Coffee.eShop/Coffee.eShop/Repositories/ProductRepository.cs
@@ -8,8 +8,8 @@ public class ProductRepository : IProductRepository
     private readonly List<Product> _products = new()
     {
         new Product { Id = 1, Name = "Americano", Price = 10.01M, Detail = "Coffee Americano", IsTrendingProduct = false, ImageUrl = "https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q29mZmVlfGVufDB8fDB8fHwy&auto=format&fit=crop&w=500&q=60" },
-        new Product { Id = 2, Name = "Cortado", Price = 10.01M, Detail = "Coffee Cortado", IsTrendingProduct = true, ImageUrl = "https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q29mZmVlfGVufDB8fDB8fHwy&auto=format&fit=crop&w=500&q=60" },
-        new Product { Id = 3, Name = "Mocha", Price = 10.01M, Detail = "Coffee Mocha", IsTrendingProduct = true, ImageUrl = "https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q29mZmVlfGVufDB8fDB8fHwy&auto=format&fit=crop&w=500&q=60" }
+        new Product { Id = 2, Name = "Cortado", Price = 10.01M, Detail = "Coffee Cortado", IsTrendingProduct = true, ImageUrl = "https://images.unsplash.com/photo-1495474472287-4d71bcdd2085?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Q29mZmVlfGVufDB8fDB8fHwy&auto=format&fit=crop&w=500&q=60" },
+        new Product { Id = 3, Name = "Mocha", Price = 10.01M, Detail = "Coffee Mocha", IsTrendingProduct = true, ImageUrl = "https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Q29mZmVlfGVufDB8fDB8fHwy&auto=format&fit=crop&w=500&q=60" }
     };
 
     //private CoffeeShopDbContext dbContext;
