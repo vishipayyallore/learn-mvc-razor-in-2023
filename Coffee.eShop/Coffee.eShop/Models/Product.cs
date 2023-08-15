70b569fe9762abfca67ffb42495d7d08d73212c9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-15 11:40:58 +0530

    Swamy/14aug coffeeshop day1 (#3)
    * Changes
    
    * Changes
    
    * Changes
    
    * Update Coffee.eShop.csproj
    
    * Changes
    
    * Create ProductRepository.cs
    
    * Update ProductRepository.cs
    
    * Update Program.cs
    
    * Create ProductsController.cs
    
    * Create Shop.cshtml
    
    * Changes
    
    * Update Coffee.eShop.csproj

---
+++ Coffee.eShop/Coffee.eShop/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Coffee.eShop.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Detail { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public decimal Price { get; set; }
+
+    public bool IsTrendingProduct { get; set; }
+}
---
+++ Coffee.eShop/Coffee.eShop/Repositories/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Coffee.eShop.ApplicationCore.Interfaces;
+using Coffee.eShop.Models;
+
+namespace Coffee.eShop.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly List<Product> _products = new()
+    {
+        new Product { Id = 1, Name = "Americano", Price = 10.01M, Detail = "Coffee Americano", IsTrendingProduct = false, ImageUrl = "https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q29mZmVlfGVufDB8fDB8fHwy&auto=format&fit=crop&w=500&q=60" },
+        new Product { Id = 2, Name = "Cortado", Price = 10.01M, Detail = "Coffee Cortado", IsTrendingProduct = true, ImageUrl = "https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q29mZmVlfGVufDB8fDB8fHwy&auto=format&fit=crop&w=500&q=60" },
+        new Product { Id = 3, Name = "Mocha", Price = 10.01M, Detail = "Coffee Mocha", IsTrendingProduct = true, ImageUrl = "https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q29mZmVlfGVufDB8fDB8fHwy&auto=format&fit=crop&w=500&q=60" }
+    };
+
+    //private CoffeeShopDbContext dbContext;
+    //public ProductRepository(CoffeeShopDbContext dbContext)
+    //{
+    //    this.dbContext = dbContext;
+    //}
+
+    public IEnumerable<Product> GetAllProducts()
+    {
+        return _products;
+    }
+
+    public Product? GetProductDetail(int id)
+    {
+        return _products.FirstOrDefault(p => p.Id == id);
+    }
+
+    public IEnumerable<Product> GetTrendingProducts()
+    {
+        return _products.Where(p => p.IsTrendingProduct);
+    }
+}
