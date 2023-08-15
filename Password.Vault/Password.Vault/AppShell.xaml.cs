b99f7b89ced8783d293438a4447b7f8802818d27
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-15 20:39:02 +0530

    Initial Version

---
+++ Password.Vault/Password.Vault/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Password.Vault
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Password.Vault/Password.Vault/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Password.Vault
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
