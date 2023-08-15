750459e6a4ef1cc907d7e68ab953fa950b26f50f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-15 23:52:42 +0530

    Changes

---
+++ Coffee.eShop/Coffee.eShop/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee.eShop.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
