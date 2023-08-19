ab1bda61f8c900a38223f6b68dec5a87425dae1b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-19 14:09:31 +0530

    Update Program.cs

--- Coffee.eShop/Coffee.eShop/Program.cs
+++ Coffee.eShop/Coffee.eShop/Program.cs
@@ -10,25 +10,39 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
+builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>(ShoppingCartRepository.GetCart);
+
 builder.Services.AddDbContext<CoffeeShopDbContext>(options =>
 {
     _ = options.UseSqlServer(builder.Configuration.GetConnectionString("CoffeeShopConnectionString"));
 });
 
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
+
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
+app.MapRazorPages();
+
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
