cf6d143008f9e63120098ef35b57be92b2e4c558
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-19 12:41:09 +0530

    Changes (#6)

---
+++ Coffee.eShop/Coffee.eShop/Models/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Coffee.eShop.Models;
+
+public class ShoppingCartItem
+{
+    public int Id { get; set; }
+
+    public Product? Product { get; set; }
+
+    public int Qty { get; set; }
+
+    public string? ShoppingCartId { get; set; }
+}
