701ecc95598e705bb3a590f234ee35215c4b0747
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-19 15:24:18 +0530

    Swamy/19aug coffee shop day4 (#7)
    * Changes
    
    * Create IShoppingCartRepository.cs
    
    * Changes
    
    * Update ShoppingCartRepository.cs
    
    * Update ShoppingCartRepository.cs
    
    * Update ShoppingCartRepository.cs
    
    * Update Program.cs
    
    * Create ShoppingCartController.cs
    
    * Create Index.cshtml
    
    * Update _Layout.cshtml
    
    * Update ShoppingCartRepository.cs
    
    * Update Detail.cshtml

---
+++ Coffee.eShop/Coffee.eShop/ApplicationCore/Interfaces/IShoppingCartRepository.cs
@@ -0,0 +1,18 @@
+﻿using Coffee.eShop.Models;
+
+namespace Coffee.eShop.ApplicationCore.Interfaces;
+
+public interface IShoppingCartRepository
+{
+    public List<ShoppingCartItem>? ShoppingCartItems { get; set; }
+
+    void AddToCart(Product product);
+
+    int RemoveFromCart(Product product);
+
+    List<ShoppingCartItem> GetShoppingCartItems();
+
+    void ClearCart();
+
+    decimal GetShoppingCartTotal();
+}
---
+++ Coffee.eShop/Coffee.eShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,61 @@
+﻿using Coffee.eShop.ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coffee.eShop.Controllers;
+
+public class ShoppingCartController : Controller
+{
+
+    private readonly IShoppingCartRepository _shoppingCartRepository;
+    private readonly IProductRepository _productRepository;
+
+    public ShoppingCartController(IShoppingCartRepository shoppingCartRepository, IProductRepository productRepository)
+    {
+        _shoppingCartRepository = shoppingCartRepository;
+        _productRepository = productRepository;
+    }
+
+    public IActionResult Index()
+    {
+        var items = _shoppingCartRepository.GetShoppingCartItems();
+
+        _shoppingCartRepository.ShoppingCartItems = items;
+
+        ViewBag.CartTotal = _shoppingCartRepository.GetShoppingCartTotal();
+
+        return View(items);
+    }
+
+    public RedirectToActionResult AddToShoppingCart(int pId)
+    {
+        var product = _productRepository.GetAllProducts().FirstOrDefault(p => p.Id == pId);
+
+        if (product is not null)
+        {
+            _shoppingCartRepository.AddToCart(product);
+
+            int cartCount = _shoppingCartRepository.GetShoppingCartItems().Count;
+
+            HttpContext.Session.SetInt32("CartCount", cartCount);
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    public RedirectToActionResult RemoveFromShoppingCart(int pId)
+    {
+        var product = _productRepository.GetAllProducts().FirstOrDefault(p => p.Id == pId);
+
+        if (product is not null)
+        {
+            _shoppingCartRepository.RemoveFromCart(product);
+
+            int cartCount = _shoppingCartRepository.GetShoppingCartItems().Count;
+
+            HttpContext.Session.SetInt32("CartCount", cartCount);
+        }
+
+        return RedirectToAction("Index");
+    }
+
+}
