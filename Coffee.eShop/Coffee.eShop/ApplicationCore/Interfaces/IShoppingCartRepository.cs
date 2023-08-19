39564b35d36b8c9dd10efffba189846cf4725864
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-19 12:47:20 +0530

    Changes

--- Coffee.eShop/Coffee.eShop/ApplicationCore/Interfaces/IShoppingCartRepository.cs
+++ Coffee.eShop/Coffee.eShop/ApplicationCore/Interfaces/IShoppingCartRepository.cs
@@ -1,5 +1,18 @@
-﻿namespace Coffee.eShop.ApplicationCore.Interfaces;
+﻿using Coffee.eShop.Models;
+
+namespace Coffee.eShop.ApplicationCore.Interfaces;
 
 public interface IShoppingCartRepository
 {
+    public List<ShoppingCartItem>? ShoppingCartItems { get; set; }
+
+    void AddToCart(Product product);
+
+    int RemoveFromCart(Product product);
+
+    List<ShoppingCartItem> GetShoppingCartItems();
+
+    void ClearCart();
+
+    decimal GetShoppingCartTotal();
 }
---
+++ Coffee.eShop/Coffee.eShop/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,95 @@
+﻿using Coffee.eShop.ApplicationCore.Interfaces;
+using Coffee.eShop.Data;
+using Coffee.eShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Coffee.eShop.Repositories;
+
+public class ShoppingCartRepository : IShoppingCartRepository
+{
+    private readonly CoffeeShopDbContext _coffeeShopDbContext;
+
+    public List<ShoppingCartItem>? ShoppingCartItems { get; set; }
+
+    public string? ShoppingCartId { get; set; }
+
+    public ShoppingCartRepository(CoffeeShopDbContext dbContext)
+    {
+        _coffeeShopDbContext = dbContext;
+    }
+
+    public static ShoppingCartRepository GetCart(IServiceProvider services)
+    {
+        ISession? session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext?.Session;
+
+        CoffeeShopDbContext context = services.GetService<CoffeeShopDbContext>() ?? throw new Exception("Error initializing coffeeshopdbcontext");
+
+        string cartId = session?.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+        session?.SetString("CartId", cartId);
+
+        return new ShoppingCartRepository(context) { ShoppingCartId = cartId };
+    }
+
+
+    public void AddToCart(Product product)
+    {
+        var shoppingCartItem = _coffeeShopDbContext.ShoppingCartItems.FirstOrDefault(s => s.Product.Id == product.Id && s.ShoppingCartId == ShoppingCartId);
+        if (shoppingCartItem == null)
+        {
+            shoppingCartItem = new ShoppingCartItem
+            {
+                ShoppingCartId = ShoppingCartId,
+                Product = product,
+                Qty = 1
+            };
+            _coffeeShopDbContext.ShoppingCartItems.Add(shoppingCartItem);
+        }
+        else
+        {
+            shoppingCartItem.Qty++;
+        }
+        _coffeeShopDbContext.SaveChanges();
+    }
+
+    public void ClearCart()
+    {
+        var cartItems = _coffeeShopDbContext.ShoppingCartItems.Where(s => s.ShoppingCartId == ShoppingCartId);
+        _coffeeShopDbContext.ShoppingCartItems.RemoveRange(cartItems);
+        _coffeeShopDbContext.SaveChanges();
+    }
+
+    public List<ShoppingCartItem> GetShoppingCartItems()
+    {
+        return ShoppingCartItems ??= _coffeeShopDbContext.ShoppingCartItems.Where(s => s.ShoppingCartId == ShoppingCartId)
+             .Include(p => p.Product).ToList();
+    }
+
+    public decimal GetShoppingCartTotal()
+    {
+        var totalCost = _coffeeShopDbContext.ShoppingCartItems.Where(s => s.ShoppingCartId == ShoppingCartId)
+              .Select(s => s.Product.Price * s.Qty).Sum();
+        return totalCost;
+    }
+
+    public int RemoveFromCart(Product product)
+    {
+        var shoppingCartItem = _coffeeShopDbContext.ShoppingCartItems.FirstOrDefault(s => s.Product.Id == product.Id && s.ShoppingCartId == ShoppingCartId);
+        var quantity = 0;
+
+        if (shoppingCartItem != null)
+        {
+            if (shoppingCartItem.Qty > 1)
+            {
+                shoppingCartItem.Qty--;
+                quantity = shoppingCartItem.Qty;
+            }
+            else
+            {
+                _coffeeShopDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+            }
+        }
+        _coffeeShopDbContext.SaveChanges();
+        return quantity;
+    }
+}
