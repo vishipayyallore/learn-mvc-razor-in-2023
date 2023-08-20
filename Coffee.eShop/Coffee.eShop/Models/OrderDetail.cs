d38b45ca0bebb767319400ae3c441c6121a53246
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-20 20:17:17 +0530

    Changes

--- Coffee.eShop/Coffee.eShop/Models/OrderDetail.cs
+++ Coffee.eShop/Coffee.eShop/Models/OrderDetail.cs
@@ -3,16 +3,16 @@
 public class OrderDetail
 {
     public int OrderDetailId { get; set; }
-    
+
     public int ProductId { get; set; }
-    
+
     public Product? Product { get; set; }
-    
+
     public int OrderId { get; set; }
-    
+
     public Order? Order { get; set; }
-    
+
     public int Quantity { get; set; }
-    
+
     public decimal Price { get; set; }
 }
