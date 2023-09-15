a1acf51417059771f10f632871c934ff87a2f243
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-15 20:24:07 +0530

    Changes

---
+++ Resorts/Resorts.Web/Controllers/VillaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resorts.Infrastructure.Data;
+
+namespace Resorts.Web.Controllers;
+
+public class VillaController : Controller
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public VillaController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    public IActionResult Index()
+    {
+        var villas = _dbContext.Villas;
+
+        return View();
+    }
+}
