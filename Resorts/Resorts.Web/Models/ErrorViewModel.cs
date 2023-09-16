c23e70821fb7bf305e39247e5b01c87dd25a3e11
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-16 08:32:01 +0530

    Swamy/15sep resorts cleanarch day3 (#14)
    * Changes
    
    * Create Index.cshtml
    
    * Update VillaController.cs
    
    * Villa List
    
    * Changes
    
    * Client Packages
    
    * Update _Layout.cshtml
    
    * Update _Layout.cshtml
    
    * Create Villa Page
    
    * Create Villa

--- Resorts/Resorts.Web/Models/ErrorViewModel.cs
+++ Resorts/Resorts.Web/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace Resorts.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace Resorts.Web.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
