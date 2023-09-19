4d66e27882730c82a17190390f9a28290ec72767
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-19 11:31:31 +0530

    Create VillaNumber.cs

---
+++ Resorts/Resorts.Domain/Entities/VillaNumber.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Resorts.Domain.Entities;
+
+public class VillaNumber
+{
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+    [Display(Name = "Villa Number")]
+    public int Villa_Number { get; set; }
+
+    [ForeignKey("Villa")]
+    public int VillaId { get; set; }
+
+    [ValidateNever]
+    public Villa Villa { get; set; }
+
+    public string? SpecialDetails { get; set; }
+}
