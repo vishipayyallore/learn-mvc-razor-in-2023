7d03c7e4380e6f169a217c841803c2317b3060e2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-19 11:43:16 +0530

    Create VillaNumberController.cs

---
+++ Resorts/Resorts.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resorts.Infrastructure.Data;
+
+namespace Resorts.Web.Controllers;
+
+public class VillaNumberController(ApplicationDbContext dbContext) : Controller
+{
+    private readonly ApplicationDbContext _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+    public IActionResult Index()
+    {
+        var villaNumbers = _dbContext.VillaNumbers;
+
+        return View(villaNumbers);
+    }
+
+}
