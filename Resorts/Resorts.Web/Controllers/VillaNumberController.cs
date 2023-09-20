8c5981598c929c70750670ef7e27f3cac43fb9c0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-20 14:18:30 +0530

    Swamy/19sep resorts cleanarch day6 (#17)
    * Create VillaNumber.cs
    
    * Villa Number Db Context
    
    * Villa Numbers Migration
    
    * Create VillaNumberController.cs
    
    * Villa Numbers List view
    
    * Update Index.cshtml
    
    * Update VillaNumberController.cs
    
    * Create Create.cshtml
    
    * Update Create.cshtml
    
    * Villa Validation
    
    * Update Index.cshtml

---
+++ Resorts/Resorts.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resorts.Domain.Entities;
+using Resorts.Infrastructure.Data;
+
+namespace Resorts.Web.Controllers;
+
+public class VillaNumberController(ApplicationDbContext dbContext) : Controller
+{
+    private readonly ApplicationDbContext _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+    public IActionResult Index()
+    {
+        var villaNumbers = _dbContext.VillaNumbers;
+
+        return View(villaNumbers);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(VillaNumber villaNumber)
+    {
+        // Method 1
+        // ModelState.Remove("Villa");
+
+        if (ModelState.IsValid)
+        {
+            _dbContext.VillaNumbers.Add(villaNumber);
+            _dbContext.SaveChanges();
+
+            TempData["success"] = "The Villa Number has been created successfully.";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(villaNumber);
+    }
+
+}
