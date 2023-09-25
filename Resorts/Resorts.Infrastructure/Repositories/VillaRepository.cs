3280cf57a80ba274b161ce2f7cb9a7c167e4b870
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-25 12:29:16 +0530

    Create VillaRepository.cs

---
+++ Resorts/Resorts.Infrastructure/Repositories/VillaRepository.cs
@@ -0,0 +1,38 @@
+﻿using Resorts.Application.Common.Interfaces;
+using Resorts.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Resorts.Infrastructure.Repositories;
+
+public class VillaRepository : IVillaRepository
+{
+    public void Add(Villa villa)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Villa> Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(Villa villa)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Save()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Villa villa)
+    {
+        throw new NotImplementedException();
+    }
+}
