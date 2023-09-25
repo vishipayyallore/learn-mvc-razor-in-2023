a17fdb2193c13035ec323149954f7bcecd50af67
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-25 12:22:54 +0530

    Create IVillaRepository.cs

---
+++ Resorts/Resorts.Application/Common/Interfaces/IVillaRepository.cs
@@ -0,0 +1,9 @@
+﻿using Resorts.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Resorts.Application.Common.Interfaces;
+
+public interface IVillaRepository
+{
+    IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null, bool tracked = false);
+}
