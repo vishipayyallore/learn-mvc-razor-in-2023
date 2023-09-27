d87a1be4eb950102d1bc1701ce596818787cd76f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-27 14:50:27 +0530

    Generic Repository

---
+++ Resorts/Resorts.Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Resorts.Application.Common.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
+
+    T? Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
+
+    void Add(T villa);
+
+    void Remove(T villa);
+}
---
+++ Resorts/Resorts.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,27 @@
+﻿using Resorts.Application.Common.Interfaces;
+using System.Linq.Expressions;
+
+namespace Resorts.Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    public void Add(T villa)
+    {
+        throw new NotImplementedException();
+    }
+
+    public T? Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(T villa)
+    {
+        throw new NotImplementedException();
+    }
+}
