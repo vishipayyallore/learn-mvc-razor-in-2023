0d9841ac4289a0c46972f5b79b7e68d0f1af3ad1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-28 11:57:55 +0530

    Code Refactoring

--- Resorts/Resorts.Application/Common/Interfaces/IVillaRepository.cs
+++ Resorts/Resorts.Application/Common/Interfaces/IVillaRepository.cs
@@ -1,19 +1,10 @@
 ﻿using Resorts.Domain.Entities;
-using System.Linq.Expressions;
 
 namespace Resorts.Application.Common.Interfaces;
 
-public interface IVillaRepository
+public interface IVillaRepository : IRepository<Villa>
 {
-    IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null);
-
-    Villa? Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null);
-
-    void Add(Villa villa);
-
     void Update(Villa villa);
-
-    void Remove(Villa villa);
 
     void Save();
 }
--- Resorts/Resorts.Infrastructure/Repositories/VillaRepository.cs
+++ Resorts/Resorts.Infrastructure/Repositories/VillaRepository.cs
@@ -1,65 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Resorts.Application.Common.Interfaces;
+﻿using Resorts.Application.Common.Interfaces;
 using Resorts.Domain.Entities;
 using Resorts.Infrastructure.Data;
-using System.Linq.Expressions;
 
 namespace Resorts.Infrastructure.Repositories;
 
-public class VillaRepository(ApplicationDbContext dbContext) : IVillaRepository
+public class VillaRepository(ApplicationDbContext dbContext) : Repository<Villa>(dbContext), IVillaRepository
 {
-    private readonly ApplicationDbContext _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
     private static readonly char[] separator = [','];
-
-    public void Add(Villa villa)
-    {
-        _ = _dbContext.Add(villa);
-    }
-
-    public Villa? Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
-    {
-        IQueryable<Villa> query = _dbContext.Set<Villa>().AsQueryable();
-
-        if (filter is not null)
-        {
-            query = query.Where(filter);
-        }
-
-        if (!string.IsNullOrWhiteSpace(includeProperties))
-        {
-            foreach (var property in includeProperties.Split(separator, StringSplitOptions.RemoveEmptyEntries))
-            {
-                query = query.Include(property);
-            }
-        }
-
-        return query.FirstOrDefault();
-    }
-
-    public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
-    {
-        IQueryable<Villa> query = _dbContext.Set<Villa>().AsQueryable();
-
-        if (filter is not null)
-        {
-            query = query.Where(filter);
-        }
-
-        if (!string.IsNullOrWhiteSpace(includeProperties))
-        {
-            foreach (var property in includeProperties.Split(separator, StringSplitOptions.RemoveEmptyEntries))
-            {
-                query = query.Include(property);
-            }
-        }
-
-        return query.ToList();
-    }
-
-    public void Remove(Villa villa)
-    {
-        _ = (_dbContext?.Remove(villa));
-    }
+    private readonly ApplicationDbContext _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
 
     public void Save()
     {
