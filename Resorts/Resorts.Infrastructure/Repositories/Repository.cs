2e5c09b3c5c2a4765ed130d996d77cf048854d14
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-28 11:51:56 +0530

    Update Repository.cs

--- Resorts/Resorts.Infrastructure/Repositories/Repository.cs
+++ Resorts/Resorts.Infrastructure/Repositories/Repository.cs
@@ -1,27 +1,70 @@
-﻿using Resorts.Application.Common.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using Resorts.Application.Common.Interfaces;
+using Resorts.Infrastructure.Data;
 using System.Linq.Expressions;
 
 namespace Resorts.Infrastructure.Repositories;
 
 public class Repository<T> : IRepository<T> where T : class
 {
-    public void Add(T villa)
+    private static readonly char[] separator = [','];
+    private readonly ApplicationDbContext _context;
+    internal DbSet<T> _dbSet;
+
+    public Repository(ApplicationDbContext context)
     {
-        throw new NotImplementedException();
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+
+        _dbSet = _context.Set<T>();
+    }
+
+    public void Add(T entity)
+    {
+        _ = _dbSet?.Add(entity);
     }
 
     public T? Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
     {
-        throw new NotImplementedException();
+        IQueryable<T> query = _dbSet;
+
+        if (filter is not null)
+        {
+            query = query.Where(filter);
+        }
+
+        if (!string.IsNullOrWhiteSpace(includeProperties))
+        {
+            foreach (var property in includeProperties.Split(separator, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(property);
+            }
+        }
+
+        return query.FirstOrDefault();
     }
 
     public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
     {
-        throw new NotImplementedException();
+        IQueryable<T> query = _dbSet;
+
+        if (filter is not null)
+        {
+            query = query.Where(filter);
+        }
+
+        if (!string.IsNullOrWhiteSpace(includeProperties))
+        {
+            foreach (var property in includeProperties.Split(separator, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(property);
+            }
+        }
+
+        return query.ToList();
     }
 
-    public void Remove(T villa)
+    public void Remove(T entity)
     {
-        throw new NotImplementedException();
+        _ = _dbSet?.Remove(entity);
     }
 }
