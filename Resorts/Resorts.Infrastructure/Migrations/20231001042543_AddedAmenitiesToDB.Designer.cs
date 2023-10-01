04a8bc6d85e8a0a35378425405493233036cc7a9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-01 09:57:25 +0530

    Amenities Database Migration

--- Resorts/Resorts.Infrastructure/Migrations/20231001042543_AddedAmenitiesToDB.Designer.cs
+++ Resorts/Resorts.Infrastructure/Migrations/20231001042543_AddedAmenitiesToDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Resorts.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using Resorts.Infrastructure.Data;
 namespace Resorts.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231001042543_AddedAmenitiesToDB")]
+    partial class AddedAmenitiesToDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
