eafbf9581fe7a0ca59c81088f4c03491533aee97
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-01 10:02:54 +0530

    Amenity Repository

---
+++ Resorts/Resorts.Application/Common/Interfaces/IAmenityRepository.cs
@@ -0,0 +1,10 @@
+﻿using Resorts.Domain.Entities;
+
+namespace Resorts.Application.Common.Interfaces;
+
+public interface IAmenityRepository : IRepository<Amenity>
+{
+    void Update(Amenity entity);
+
+    void Save();
+}
---
+++ Resorts/Resorts.Infrastructure/Repositories/AmenityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Resorts.Application.Common.Interfaces;
+using Resorts.Domain.Entities;
+using Resorts.Infrastructure.Data;
+
+namespace Resorts.Infrastructure.Repositories;
+
+public class AmenityRepository(ApplicationDbContext dbContext) : Repository<Amenity>(dbContext), IAmenityRepository
+{
+    private static readonly char[] separator = [','];
+    private readonly ApplicationDbContext _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+    public void Save()
+    {
+        _ = _dbContext.SaveChanges();
+    }
+
+    public void Update(Amenity entity)
+    {
+        _ = _dbContext.Amenities.Update(entity);
+    }
+}
