8d0ecbd5ca8b255ad6d1bd02d3739e58312d6e8d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-08 20:21:17 +0530

    View Models

---
+++ Resorts/Resorts.Web/ViewModels/RegisterVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Resorts.Web.ViewModels;
+
+public class RegisterVM
+{
+    public required string Email { get; set; }
+
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password))]
+    [Display(Name = "Confirm password")]
+    public required string ConfirmPassword { get; set; }
+
+    public required string Name { get; set; }
+
+    [Display(Name = "Phone Number")]
+    public string? PhoneNumber { get; set; }
+
+    public string? RedirectUrl { get; set; }
+
+    //public string? Role { get; set; }
+
+    //[ValidateNever]
+    //public IEnumerable<SelectListItem>? RoleList { get; set; }
+}
