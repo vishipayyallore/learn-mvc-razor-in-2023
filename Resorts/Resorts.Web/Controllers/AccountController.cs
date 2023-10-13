92bbef1e625db9af8cf62eca5b99b66ea20c2442
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-13 10:22:11 +0530

    Swamy/12oct resorts cleanarch day17 (#30)
    * Updated Nuget Packages
    
    * Create AccountController.cs
    
    * Login Page

---
+++ Resorts/Resorts.Web/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Resorts.Application.Common.Interfaces;
+using Resorts.Domain.Entities;
+using Resorts.Web.ViewModels;
+
+namespace Resorts.Web.Controllers;
+
+public class AccountController(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager) : Controller
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+    private readonly UserManager<ApplicationUser> _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+    private readonly SignInManager<ApplicationUser> _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+    private readonly RoleManager<IdentityRole> _roleManager = roleManager ?? throw new ArgumentNullException(nameof(roleManager));
+
+    public IActionResult Login(string? returnUrl)
+    {
+        returnUrl ??= Url.Content("~/");
+
+        LoginVM loginVM = new()
+        {
+            Email = string.Empty,
+            Password = string.Empty,
+            RedirectUrl = returnUrl,
+        };
+
+        return View(loginVM);
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
