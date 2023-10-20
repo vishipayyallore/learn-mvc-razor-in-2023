8ee365bc3ed345ebd2e11389952620bfecc175b3
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-20 12:21:42 +0530

    Create BookingController.cs

---
+++ Resorts/Resorts.Web/Controllers/BookingController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resorts.Application.Common.Interfaces;
+using Resorts.Domain.Entities;
+
+namespace Resorts.Web.Controllers;
+
+public class BookingController(IUnitOfWork unitOfWork) : Controller
+{
+    private readonly IUnitOfWork _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult FinalizeBooking(int villaId, DateOnly checkInDate, int nights)
+    {
+        Booking booking = new()
+        {
+            VillaId = villaId,
+            Villa = _unitOfWork.Villa.Get(u => u.Id == villaId, includeProperties: "VillaAmenity"),
+            CheckInDate = checkInDate,
+            Nights = nights,
+            CheckOutDate = checkInDate.AddDays(nights),
+            UserId = string.Empty,
+            Phone = string.Empty,
+            Email = string.Empty,
+            Name = string.Empty,
+            TotalCost = 0,
+            BookingDate = DateTime.UtcNow
+        };
+        booking.TotalCost = booking.Villa.Price * nights;
+
+        return View(booking);
+    }
+}
