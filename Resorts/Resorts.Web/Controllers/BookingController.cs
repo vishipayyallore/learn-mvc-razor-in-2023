399c03059bfea0fd85caa260c0c80f1da0b07447
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-20 20:57:11 +0530

    Update BookingController.cs

--- Resorts/Resorts.Web/Controllers/BookingController.cs
+++ Resorts/Resorts.Web/Controllers/BookingController.cs
@@ -1,9 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Resorts.Application.Common.Interfaces;
 using Resorts.Domain.Entities;
 using System.Security.Claims;
+using WhiteLagoon.Application.Common.Utility;
 
 namespace Resorts.Web.Controllers;
 
@@ -42,5 +42,51 @@ public class BookingController(IUnitOfWork unitOfWork) : Controller
         booking.TotalCost = booking.Villa.Price * nights;
 
         return View(booking);
+    }
+
+    [Authorize]
+    [HttpPost]
+    public IActionResult FinalizeBooking(Booking booking)
+    {
+        var villa = _unitOfWork.Villa.Get(r => r.Id == booking.VillaId);
+        booking.TotalCost = villa!.Price * booking.Nights;
+
+        booking.Status = SD.StatusPending;
+        booking.BookingDate = DateTime.Now;
+
+        _unitOfWork.Booking.Add(booking);
+        _unitOfWork.Save();
+
+        return RedirectToAction(nameof(BookingConfirmation), new { bookingId = booking.Id });
+
+        //if (!_villaService.IsVillaAvailableByDate(villa.Id, booking.Nights, booking.CheckInDate))
+        //{
+        //    TempData["error"] = "Room has been sold out!";
+        //    //no rooms available
+        //    return RedirectToAction(nameof(FinalizeBooking), new
+        //    {
+        //        villaId = booking.VillaId,
+        //        checkInDate = booking.CheckInDate,
+        //        nights = booking.Nights
+        //    });
+        //}
+
+        //_bookingService.CreateBooking(booking);
+
+        //var domain = Request.Scheme + "://" + Request.Host.Value + "/";
+
+        //var options = _paymentService.CreateStripeSessionOptions(booking, villa, domain);
+
+        //var session = _paymentService.CreateStripeSession(options);
+
+        //_bookingService.UpdateStripePaymentID(booking.Id, session.Id, session.PaymentIntentId);
+        //Response.Headers.Add("Location", session.Url);
+        //return new StatusCodeResult(303);
+    }
+
+    [Authorize]
+    public IActionResult BookingConfirmation(int bookingId)
+    {
+        return View(bookingId);
     }
 }
