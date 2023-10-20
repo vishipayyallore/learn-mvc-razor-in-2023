099954328d29357d50b6f3badbd1824c4c1f2368
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-20 12:09:33 +0530

    Create BookingRepository.cs

---
+++ Resorts/Resorts.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Resorts.Application.Common.Interfaces;
+using Resorts.Domain.Entities;
+using Resorts.Infrastructure.Data;
+
+namespace Resorts.Infrastructure.Repositories;
+
+public class BookingRepository(ApplicationDbContext dbContext) : Repository<Booking>(dbContext), IBookingRepository
+{
+    private readonly ApplicationDbContext _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+    public void Update(Booking entity)
+    {
+        _dbContext.Bookings.Update(entity);
+    }
+}
