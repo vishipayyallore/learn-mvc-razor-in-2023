dc137351b6a3cbe4e19208b9d3230272c8c77588
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-20 21:01:08 +0530

    Update BookingController.cs

--- Resorts/Resorts.Web/Controllers/BookingController.cs
+++ Resorts/Resorts.Web/Controllers/BookingController.cs
@@ -58,30 +58,6 @@ public class BookingController(IUnitOfWork unitOfWork) : Controller
         _unitOfWork.Save();
 
         return RedirectToAction(nameof(BookingConfirmation), new { bookingId = booking.Id });
-
-        //if (!_villaService.IsVillaAvailableByDate(villa.Id, booking.Nights, booking.CheckInDate))
-        //{
-        //    TempData["error"] = "Room has been sold out!";
-        //    //no rooms available
-        //    return RedirectToAction(nameof(FinalizeBooking), new
-        //    {
-        //        villaId = booking.VillaId,
-        //        checkInDate = booking.CheckInDate,
-        //        nights = booking.Nights
-        //    });
-        //}
-
-        //_bookingService.CreateBooking(booking);
-
-        //var domain = Request.Scheme + "://" + Request.Host.Value + "/";
-
-        //var options = _paymentService.CreateStripeSessionOptions(booking, villa, domain);
-
-        //var session = _paymentService.CreateStripeSession(options);
-
-        //_bookingService.UpdateStripePaymentID(booking.Id, session.Id, session.PaymentIntentId);
-        //Response.Headers.Add("Location", session.Url);
-        //return new StatusCodeResult(303);
     }
 
     [Authorize]
