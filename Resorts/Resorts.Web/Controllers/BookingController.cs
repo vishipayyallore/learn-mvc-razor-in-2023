251f3d1541823a3650803914219cc0c90be19764
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-23 19:19:34 +0530

    BookingConfirmation

--- Resorts/Resorts.Web/Controllers/BookingController.cs
+++ Resorts/Resorts.Web/Controllers/BookingController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Resorts.Application.Common.Interfaces;
 using Resorts.Domain.Entities;
+using Stripe.Checkout;
 using System.Security.Claims;
 using WhiteLagoon.Application.Common.Utility;
 
@@ -57,12 +58,62 @@ public class BookingController(IUnitOfWork unitOfWork) : Controller
         _unitOfWork.Booking.Add(booking);
         _unitOfWork.Save();
 
-        return RedirectToAction(nameof(BookingConfirmation), new { bookingId = booking.Id });
+        var domain = Request.Scheme + "://" + Request.Host.Value + "/";
+        var options = new SessionCreateOptions
+        {
+            LineItems = new List<SessionLineItemOptions>(),
+            Mode = "payment",
+            SuccessUrl = domain + $"booking/BookingConfirmation?bookingId={booking.Id}",
+            CancelUrl = domain + $"booking/FinalizeBooking?villaId={booking.VillaId}&checkInDate={booking.CheckInDate}&nights={booking.Nights}",
+        };
+
+        options.LineItems.Add(new SessionLineItemOptions
+        {
+            PriceData = new SessionLineItemPriceDataOptions
+            {
+                UnitAmount = (long)(booking.TotalCost * 100),
+                Currency = "inr",
+                ProductData = new SessionLineItemPriceDataProductDataOptions
+                {
+                    Name = villa.Name
+                },
+            },
+            Quantity = 1,
+        });
+
+        var service = new SessionService();
+        Session session = service.Create(options);
+
+        _unitOfWork.Booking.UpdateStripePaymentID(booking.Id, session.Id, session.PaymentIntentId);
+        _unitOfWork.Save();
+
+        Response?.Headers?.Append("Location", session.Url);
+
+        return new StatusCodeResult(303);
     }
 
     [Authorize]
     public IActionResult BookingConfirmation(int bookingId)
     {
+        Booking bookingFromDb = _unitOfWork.Booking.Get(u => u.Id == bookingId, includeProperties: "User,Villa");
+
+        if (bookingFromDb.Status == SD.StatusPending)
+        {
+            //this is a pending order, we need to confirm if payment was successful
+
+            var service = new SessionService();
+            Session session = service.Get(bookingFromDb.StripeSessionId);
+
+            if (session.PaymentStatus == "paid")
+            {
+                _unitOfWork.Booking.UpdateStatus(bookingFromDb.Id, SD.StatusApproved);
+
+                _unitOfWork.Booking.UpdateStripePaymentID(bookingFromDb.Id, session.Id, session.PaymentIntentId);
+
+                _unitOfWork.Save();
+            }
+        }
+
         return View(bookingId);
     }
 }
