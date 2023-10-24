2d269040100c1a14904829a18826beab609e9557
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-24 08:50:55 +0530

    Swamy/23oct resorts cleanarch day22 (#37)
    * Update IBookingRepository.cs
    
    * Payment
    
    * Changes
    
    * Update Program.cs
    
    * BookingConfirmation

--- Resorts/Resorts.Application/Common/Interfaces/IBookingRepository.cs
+++ Resorts/Resorts.Application/Common/Interfaces/IBookingRepository.cs
@@ -5,4 +5,8 @@ namespace Resorts.Application.Common.Interfaces;
 public interface IBookingRepository : IRepository<Booking>
 {
     void Update(Booking entity);
+
+    void UpdateStatus(int bookingId, string bookingStatus, int villaNumber = 0);
+
+    void UpdateStripePaymentID(int bookingId, string sessionId, string paymentIntentId);
 }
