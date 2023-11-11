50c4bc42c57b375d3ae0b4689a2fcd663e19873d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-11 12:37:45 +0530

    Update BookingController.cs

--- Resorts/Resorts.Web/Controllers/BookingController.cs
+++ Resorts/Resorts.Web/Controllers/BookingController.cs
@@ -122,15 +122,35 @@ public class BookingController(IUnitOfWork unitOfWork) : Controller
     {
         Booking bookingFromDb = _unitOfWork.Booking.Get(u => u.Id == bookingId, includeProperties: "User,Villa");
 
-        //if (bookingFromDb.VillaNumber == 0 && bookingFromDb.Status == SD.StatusApproved)
-        //{
-        //    var availableVillaNumber = AssignAvailableVillaNumberByVilla(bookingFromDb.VillaId);
+        if (bookingFromDb?.VillaNumber == 0 && bookingFromDb.Status == SD.StatusApproved)
+        {
+            var availableVillaNumber = AssignAvailableVillaNumberByVilla(bookingFromDb.VillaId);
 
-        //    bookingFromDb.VillaNumbers = _villaNumberService.GetAllVillaNumbers().Where(u => u.VillaId == bookingFromDb.VillaId
-        //    && availableVillaNumber.Any(x => x == u.Villa_Number)).ToList();
-        //}
+            bookingFromDb.VillaNumbers = _unitOfWork.VillaNumber.GetAll(u => u.VillaId == bookingFromDb.VillaId
+                && availableVillaNumber.Any(x => x == u.Villa_Number)).ToList();
+        }
 
         return View(bookingFromDb);
+    }
+
+    private List<int> AssignAvailableVillaNumberByVilla(int villaId)
+    {
+        List<int> availableVillaNumbers = [];
+
+        var villaNumbers = _unitOfWork.VillaNumber.GetAll(u => u.VillaId == villaId);
+
+        var checkedInVilla = _unitOfWork.Booking
+            .GetAll(u => u.VillaId == villaId && u.Status == SD.StatusCheckedIn)
+            .Select(u => u.VillaNumber);
+
+        foreach (var villaNumber in villaNumbers)
+        {
+            if (!checkedInVilla.Contains(villaNumber.Villa_Number))
+            {
+                availableVillaNumbers.Add(villaNumber.Villa_Number);
+            }
+        }
+        return availableVillaNumbers;
     }
 
     #region API Calls
